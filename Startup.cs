1e6ecc31b2443928d56e78c09a0199ae14dce4b2
Mohammad Mhsoon <mmhsoon@hotmail.com>
2020-09-17 16:35:54 +0200

    added migrations not success

--- Startup.cs
+++ Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -30,6 +31,7 @@ namespace MyBlog
                 var connectionString = configuration.GetConnectionString("BlogDataContext");
                 options.UseSqlServer(connectionString);
             });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
